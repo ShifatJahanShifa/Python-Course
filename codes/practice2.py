99593b81e5e0269ff3c4e7547b5300cd950fe630
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-04-15 21:52:21 +0600

    nothing is related, just for practice

---
+++ codes/practice2.py
@@ -0,0 +1,20 @@
+print('nothing serious! just some random stuffs that i\'ve learned')
+print('shifa ' + 'is ' + 'a good ' + 'girl')
+name = 'shifa'
+word = 'is a good'
+word2 = 'girl'
+
+print(name, word, word2)
+value1 = 12
+value2 = 4.5
+
+print(type(value1))
+print(type(value2))
+
+print(value1/7)
+print(value1//7)
+
+result = value1 + value2
+print(result)
+print(result*2)
+print(result**2)
