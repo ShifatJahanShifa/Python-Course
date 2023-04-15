a645c5d6b5a1c72df3975ae5159ee48127daf32f
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-04-15 21:39:24 +0600

    a real story -.-

---
+++ codes/practice.py
@@ -0,0 +1,15 @@
+name = "Shifa"
+roll = 1
+gotMarks = 14.5
+comment = 'poor marks'
+
+print("what is the name of the student? ")
+print("name: "+name)
+
+print("what is the roll number of the student? ")
+print("roll: " + str(roll))
+
+print("what is the marks of the student? ")
+print("marks: " + str(gotMarks) + ' / ' + str(20))
+
+print('comment: ' + comment + ' ! ☹')
