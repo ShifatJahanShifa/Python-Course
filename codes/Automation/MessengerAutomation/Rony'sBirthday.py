1df78bd3cf87cb4a1948ca1c475a4598404c6bd6
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-06-03 00:37:26 +0600

    wished rony by running this script🥰

---
+++ codes/Automation/MessengerAutomation/Rony'sBirthday.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+
+time.sleep(2)
+pyautogui.typewrite('May you achieve your goals in life and become successful in every step. Stay healthy and stout.')
+pyautogui.press('enter')
+pyautogui.typewrite('Be happy always and have a good day. Happy Birthday, bondhu.')
+time.sleep(1)
+pyautogui.press('enter')
