68004de5e7cb8cea3a8750273b032c2f74d9ef36
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-06-08 17:26:00 +0600

    indexing, negative indexing, slice

---
+++ codes/String/basic-1.py
@@ -0,0 +1,14 @@
+fruit = 'PineApple'
+
+#indexing 
+print(fruit[2])
+
+#negative indexing
+print(fruit[-1])
+print(fruit[-2])
+
+#slice or substring
+print(fruit[1:4])
+print(fruit[:4])
+print(fruit[4:])
+print(fruit[:4]+fruit[4:])
