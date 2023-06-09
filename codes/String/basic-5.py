ac8cbbcd3514da6b25e263c91fbbc753236f4f37
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-06-10 00:01:30 +0600

    join & split method

---
+++ codes/String/basic-5.py
@@ -0,0 +1,13 @@
+#join method for string concatenation
+initialString=', '
+fruits = ['Mango','Banana','Jack-fruit','Lichi','Pineapple']
+result = initialString.join(fruits)
+print(result)
+
+#split method
+sentence = 'I love flowers specially roses are my favorite'
+list=sentence.split()
+print(list)
+sentence = 'Mango, Banana, Jack-fruit, Lichi, Pineapple'
+list = sentence.split(',')
+print(list)
