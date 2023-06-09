0dd4aa4e0d86015ef3bfeabf1f9be6bab44e727e
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-06-09 21:50:10 +0600

    short script to validate input in any case

---
+++ codes/String/Inputvalidity.py
@@ -0,0 +1,15 @@
+#just a scrpit to easily validate user input. 
+#i wish i could use this part in my SPL project to make project more dynamic
+
+def validateAnswer(answer):
+  if answer.lower()=='yes':
+    return True
+  return False
+  
+print('Do you think 2+2=4?')
+answer=input()
+if validateAnswer(answer):
+  print('Your assumption is right!')
+else:
+  print('Noo, you guessed it wrong!')
+  
