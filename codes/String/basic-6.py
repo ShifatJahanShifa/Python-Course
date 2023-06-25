d7e36653b8e52e5c4d61625e4da2e6e09d35e062
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-06-25 14:25:35 +0600

    foramtting

--- codes/String/basic-6.py
+++ codes/String/basic-6.py
@@ -13,3 +13,11 @@ baseIncome = 1020
 tax = .567
 totalIncome =baseIncome*tax
 print('base income = {}, tax = {}, totalIncome = {:.2f}'.format(baseIncome,tax,totalIncome))
+
+#formatting with double value
+
+def celciusToFarenheight(c): 
+  return (c/5)*9+32
+  
+for x in range(1,10):
+  print('{:>3} degree celcius | {:>6} degree farenheight'.format(x,celciusToFarenheight(x)))
