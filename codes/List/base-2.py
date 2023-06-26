6f0adb45aa6a8e7df2c1a1382734eafa7dc90f74
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-06-26 20:27:36 +0600

    list, len, indexing, in keyword, slicing, concatenation

---
+++ codes/List/base-2.py
@@ -0,0 +1,11 @@
+#list data type
+
+list = ['i','am','shifa','roll','1301']
+print(list)
+print(len(list))
+print('shifa' in list)
+print('jahan' in list)
+print(list[4])
+sublist = list[1:3]
+print(sublist)
+print(list+sublist)
